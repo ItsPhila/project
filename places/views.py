f7970444a596234d2caf5d0aca06e2f6941748b4
Phila <uno>
2020-02-07 14:06:13 +0300

    twelveth commit

--- places/views.py
+++ places/views.py
@@ -12,3 +12,19 @@ def index(request):
     context = {"all_places" : all_places}
     return render(request,'places/index.html',context)
 
+@login_required(redirect_field_name='/login/')
+def addplace(request):
+    if request.method == 'POST':
+        form = AddPlace(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            form.save()
+            form = AddPlace()
+            return redirect('Places:index')
+
+    else:
+        form = AddPlace()  
+    context = {"form":form}
+    return render(request,'places/addplace.html',context)
+
