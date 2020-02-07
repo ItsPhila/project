761b9e5fea9d3d6cef071115ec2774fddddb3e9b
Phila <uno>
2020-02-07 14:09:07 +0300

    fifteen commit

--- places/urls.py
+++ places/urls.py
@@ -2,6 +2,8 @@ from .views import addplace, index, detail
 from django.urls import path
 app_name = 'Place'
 urlpatterns = [
-
+    path('', index, name ='index'),
+    path('addplace', addplace, name ='addplace'),
+    path('<int:place_id>', detail, name ="detail"),
     
 ]
